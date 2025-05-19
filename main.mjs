4f167a9a9895c2779e23d04f62d4650bede44102
22278rp <22278rp@hvhs.school.nz>
2025-05-19 00:27:41 +0000

    First DAY

--- main.mjs
+++ main.mjs
@@ -1,1 +1,32 @@
+/**************************************************************/
+// main.mjs
+// Main entry for index.html
+// Written by Ryan Parks, Term 2 2025
+/**************************************************************/
+const COL_C = 'white';	    // These two const are part of the coloured 	
+const COL_B = '#CD7F32';	//  console.log for functions scheme
+console.log('%c main.mjs',
+    'color: blue; background-color: white;');
+
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the constants & functions required from script module
+import { fb_initialise }
+    from './script.mjs';
+window.fb_initialise = fb_initialise;
+import { fb_authenticate }
+    from './script.mjs';
+window.fb_authenticate = fb_authenticate;
+import { fb_WriteRec }
+    from './script.mjs';
+window.fb_WriteRec = fb_WriteRec;
+
+/**************************************************************/
+// index.html main code
+/**************************************************************/
+
+/**************************************************************/
+//   END OF CODE
+/**************************************************************/
 
--- script.mjs
+++ script.mjs
@@ -1,1 +1,100 @@
+//**************************************************************/
+// Script.mjs
+// Sal's Strawberries
+// Written by Ryan Parks, Term 2 2025
+//
+// All variables & function begin with fb_  all const with FB_
+// Diagnostic code lines have a comment appended to them //DIAG
+/**************************************************************/
+const COL_C = 'white';	    // These two const are part of the coloured 	
+const COL_B = 'pink';	//  console.log for functions scheme
+console.log('%c script.mjs',
+    'color: blue; background-color: white;');
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the methods you want to call from the firebase modules
+import { initializeApp }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { ref, set }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+/**************************************************************/
+// EXPORT FUNCTIONS
+// List all the functions called by code or html outside of this module
+/**************************************************************/
+
+export { fb_initialise, fb_authenticate, fb_WriteRec }
+
+function fb_initialise() {
+    console.log('%c fb_initialise(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    const firebaseConfig =
+    {
+        apiKey: "AIzaSyAQ3Qc6Ej_4YvNXCAjqsfLoA8p75j3R7-8",
+        authDomain: "comp2025-ryan-parks.firebaseapp.com",
+        databaseURL: "https://comp2025-ryan-parks-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "comp2025-ryan-parks",
+        storageBucket: "comp2025-ryan-parks.firebasestorage.app",
+        messagingSenderId: "73072219046",
+        appId: "1:73072219046:web:7608445213a3fd3e973567",
+        measurementId: "G-R89L1J8Z4D"
+    };
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const firebaseGameDB = getDatabase(app);
+    console.info(firebaseGameDB);
+}
+
+function fb_authenticate() {
+    console.log('%c fb_authenticate(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    const AUTH = getAuth();
+    const PROVIDER = new GoogleAuthProvider();
+    // The following makes Google ask the user to select the account
+    PROVIDER.setCustomParameters({
+        prompt: 'select_account'
+    });
+
+    signInWithPopup(AUTH, PROVIDER).then((result) => {
+        //✅ Code for a successful authentication goes here
+        console.log("Authenticated");
+    })
+
+        .catch((error) => {
+            //❌ Code for an authentication error goes here
+            console.log("ERROR!!!!!!!!");
+
+        });
+    //var userId = result.User;
+}
+
+function fb_WriteRec() {
+    console.log('%c fb_WriteRec(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    const DB = getDatabase()
+    const dbReference= ref(DB, "Test/Userdata");
+
+    var name = document.getElementById("name").value;
+    var favoriteFruit = document.getElementById("favoriteFruit").value;
+    var fruitQuantity = document.getElementById("fruitQuantity").value;
+
+    set(dbReference, {name, favoriteFruit, fruitQuantity}).then(() => {
+
+        //✅ Code for a successful write goes here
+        console.log("success write");
+
+    }).catch((error) => {
+
+        //❌ Code for a write error goes here
+         console.log("fail write");
+         console.log(error);
+
+    });
+}
+
+
+
+
+
 
